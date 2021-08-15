e1d7579c49c54669c3aeba348f62e16ab4a3ee26
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-08-15 13:45:38 +0530

    updated

--- startProject/mysite/mysite/settings.py
+++ startProject/mysite/mysite/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jd2(-v)d!ha=ixs2pt%a&2##upn9wva@#%e)5990)17w$rhob^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
